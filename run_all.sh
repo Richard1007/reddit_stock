6f98a18e4756460b6bcea0bd5e71d8e0e82d55c7
Richard1007 <hp1397@nyu.edu>
2025-06-16 22:05:21 -0400

    update

--- run_all.sh
+++ run_all.sh
@@ -3,4 +3,5 @@
 python reddit_search.py -s "Daily Discussion Thread for June 12" -l 1 -o relevance
 python reddit_search.py -s "Daily Discussion Thread for June 11" -l 1 -o relevance
 python reddit_search.py -s "Daily Discussion Thread for June 10" -l 1 -o relevance
-python reddit_search.py -s "Daily Discussion Thread for June 09" -l 1 -o relevance
+python reddit_search.py -s "Daily Discussion Thread for June 09" -l 1 -o relevance 
+python reddit_search.py -s "Daily Discussion Thread for June 08" -l 1 -o relevance
